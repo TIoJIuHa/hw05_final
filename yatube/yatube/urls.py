00136539a261c34f3b8971486eb6526e676eecc7
Полина Елистратова <p.elistratova03@mail.ru>
2022-09-04 18:47:53 +0300

    Переопределен кастомный шаблон ошибки 500

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -3,7 +3,6 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-handler404 = 'core.views.page_not_found'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +11,11 @@ urlpatterns = [
     path('about/', include('about.urls', namespace='about')),
     path('', include('posts.urls', namespace='posts')),
 ]
+
+handler404 = 'core.views.page_not_found'
+handler403 = 'core.views.csrf_failure'
+handler500 = 'core.views.server_failure'
+
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
