3f04ea0431d260dfe4ced6293fa9a848e39207a0
Полина Елистратова <p.elistratova03@mail.ru>
2022-09-07 23:28:29 +0300

    Установлен django-debug-toolbar и добавлено адаптивное меню

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,6 +16,8 @@ handler403 = 'core.views.csrf_failure'
 handler500 = 'core.views.server_failure'
 
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
